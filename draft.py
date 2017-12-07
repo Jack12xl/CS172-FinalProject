82bd43b37be38e4aa0e1b172ad18fa5aaa104929
maym <maym@shanghaitech.edu.cn>
2017-12-08 01:35:57 +0800

    normalized SIFT, all BOW extracted

--- draft.py
+++ draft.py
@@ -1,29 +1,33 @@
 import cv2
 import numpy as np
+
 # from matplotlib import pyplot as plt
 #sift
-def siftExtrator(poseId):
+def siftExtrator(poseName,poseId):
+	print(poseName)
 	descriptors=np.zeros((1,128))
 	for i in range(10):
-		img=cv2.imread('./hand-reader-dataset/%c/0000%d.jpg'%(poseId,i))
+		img=cv2.imread('./hand-reader-dataset/%c/0%d00%d.jpg'%(poseName,poseId,i))
 		# print('./hand-reader-dataset/%c/0000%d.jpg'%(poseId,i))
 		gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 		sift=cv2.xfeatures2d.SIFT_create()
 		kp,des=sift.detectAndCompute(gray,None)
+		des=des/des.sum(axis=0,dtype='float')
 		descriptors=np.concatenate((descriptors,des))
 		# print(descriptors.shape)
 		img=cv2.drawKeypoints(gray,kp,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-		cv2.imwrite('./test/sift%c%d.jpg'%(poseId,i),img)
+		cv2.imwrite('./test/sift%c%d.jpg'%(poseName,i),img)
 	for i in range(11,50):
-		img=cv2.imread('./hand-reader-dataset/%c/000%d.jpg'%(poseId,i))
+		img=cv2.imread('./hand-reader-dataset/%c/0%d0%d.jpg'%(poseName,poseId,i))
 		# print('./hand-reader-dataset/%c/000%d.jpg'%(poseId,i))
 		gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 		sift=cv2.xfeatures2d.SIFT_create()
 		kp,des=sift.detectAndCompute(gray,None)
+		des=des/des.sum(axis=0,dtype='float')
 		descriptors=np.concatenate((descriptors,des))
 
 		img=cv2.drawKeypoints(gray,kp,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-		cv2.imwrite('./test/sift%c%d.jpg'%(poseId,i),img)
+		cv2.imwrite('./test/sift%c%d.jpg'%(poseName,i),img)
 	# print(descriptors.shape)
 	return descriptors
 
@@ -38,13 +42,26 @@ def Kmeans(descriptor):
 	compactness = np.array(compactness)
 	labels = np.array(labels)
 	centers = np.array(centers)
-	print compactness
+	print(compactness)
 	return centers
 
 
+bagOfWords=[]
+for i in range(4):
+	Descriptor = siftExtrator(chr(ord('A')+i),i)
+	bagOfWords.append(Kmeans(Descriptor))
+Descriptor = siftExtrator('G',4)
+bagOfWords.append(Kmeans(Descriptor))
+Descriptor = siftExtrator('H',5)
+bagOfWords.append(Kmeans(Descriptor))
+Descriptor = siftExtrator('I',6)
+bagOfWords.append(Kmeans(Descriptor))
+Descriptor = siftExtrator('L',7)
+bagOfWords.append(Kmeans(Descriptor))
+Descriptor = siftExtrator('V',8)
+bagOfWords.append(Kmeans(Descriptor))
+Descriptor = siftExtrator('Y',9)
+bagOfWords.append(Kmeans(Descriptor))
 
-Descriptor = siftExtrator('A')
-
-Kmeans(Descriptor)
 
 
