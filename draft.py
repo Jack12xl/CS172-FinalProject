7087833820dd92881e2625e4e9898db5c10de8b8
maym <maym@shanghaitech.edu.cn>
2017-12-07 23:02:59 +0800

    fixed sift function

--- draft.py
+++ draft.py
@@ -1,30 +1,32 @@
 import cv2
+import os
 import numpy as np
 
-def siftExtrator(poseId):
+def siftExtrator(poseName,poseId):
+	# os.makedirs('./test')
 	descriptors=np.zeros((1,128))
 	for i in range(10):
-		img=cv2.imread('./Hand-Reader-Dataset/hand-reader-dataset/%c/0000%d.jpg'%(poseId,i))
-		print('./Hand-Reader-Dataset/hand-reader-dataset/%c/0000%d.jpg'%(poseId,i))
+		img=cv2.imread('./hand-reader-dataset/%c/0%d00%d.jpg'%(poseName,poseId,i))
+		print('./hand-reader-dataset/%c/0%d00%d.jpg'%(poseName,poseId,i))
 		gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 		sift=cv2.xfeatures2d.SIFT_create()
 		kp,des=sift.detectAndCompute(gray,None)
 		descriptors=np.concatenate((descriptors,des))
 
 		img=cv2.drawKeypoints(gray,kp,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-		cv2.imwrite('./test/sift%c%d.jpg'%(poseId,i),img)
+		cv2.imwrite('./test/sift%c%d.jpg'%(poseName,i),img)
 	for i in range(11,50):
-		img=cv2.imread('./Hand-Reader-Dataset/hand-reader-dataset/%c/000%d.jpg'%(poseId,i))
-		print('./Hand-Reader-Dataset/hand-reader-dataset/%c/000%d.jpg'%(poseId,i))
+		img=cv2.imread('./hand-reader-dataset/%c/0%d0%d.jpg'%(poseName,poseId,i))
+		print('./hand-reader-dataset/%c/0%d0%d.jpg'%(poseName,poseId,i))
 		gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 		sift=cv2.xfeatures2d.SIFT_create()
 		kp,des=sift.detectAndCompute(gray,None)
 		descriptors=np.concatenate((descriptors,des))
 
 		img=cv2.drawKeypoints(gray,kp,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-		cv2.imwrite('./test/sift%c%d.jpg'%(poseId,i),img)
-		print(descriptors.shape)
+		cv2.imwrite('./test/sift%c%d.jpg'%(poseName,i),img)
+		print(des.shape)
 	return descriptors
 
-siftExtrator('A')
+siftExtrator('C',2)
 
