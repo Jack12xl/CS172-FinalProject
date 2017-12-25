f54f92d9c074d23507f67b7e8adeaa23fbd190ed
maym <maym@shanghaitech.edu.cn>
2017-12-25 21:05:55 +0800

    1st version

--- HandDetection+Game.py
+++ HandDetection+Game.py
@@ -90,29 +90,29 @@ while(cap.isOpened()):
 	# # cv2.imshow('marked',img)
 	# cv2.imwrite('faceSubtraction.jpg',img)
 	# Blur the image
-	blur = cv2.blur(img,(5,5))
-	#Convert to HSV color space
-	hsv = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
-	#Create a binary image with where white will be skin colors and rest is black
-	mask2 = cv2.inRange(hsv,np.array([0,70,50]),np.array([20,190,255]))
-	#Kernel matrices for morphological transformation
-	kernel_square = np.ones((11,11),np.uint8)
-	kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-	#Perform morphological transformations to filter out the background noise
-	#Dilation increase skin color area
-	#Erosion increase skin color area
-	dilation = cv2.dilate(mask2,kernel_ellipse,iterations = 1)
-	erosion = cv2.erode(dilation,kernel_square,iterations = 1)
-	dilation2 = cv2.dilate(erosion,kernel_ellipse,iterations = 1)
-	filtered = cv2.medianBlur(dilation2,5)
-	kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(8,8))
-	dilation2 = cv2.dilate(filtered,kernel_ellipse,iterations = 1)
-	kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-	dilation3 = cv2.dilate(filtered,kernel_ellipse,iterations = 1)
-	median = cv2.medianBlur(dilation2,5)
-	ret,thresh = cv2.threshold(median,10,1,cv2.THRESH_BINARY)
+	# blur = cv2.blur(img,(5,5))
+	# #Convert to HSV color space
+	# hsv = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
+	# #Create a binary image with where white will be skin colors and rest is black
+	# mask2 = cv2.inRange(hsv,np.array([0,70,50]),np.array([20,190,255]))
+	# #Kernel matrices for morphological transformation
+	# kernel_square = np.ones((11,11),np.uint8)
+	# kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+	# #Perform morphological transformations to filter out the background noise
+	# #Dilation increase skin color area
+	# #Erosion increase skin color area
+	# dilation = cv2.dilate(mask2,kernel_ellipse,iterations = 1)
+	# erosion = cv2.erode(dilation,kernel_square,iterations = 1)
+	# dilation2 = cv2.dilate(erosion,kernel_ellipse,iterations = 1)
+	# filtered = cv2.medianBlur(dilation2,5)
+	# kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(8,8))
+	# dilation2 = cv2.dilate(filtered,kernel_ellipse,iterations = 1)
+	# kernel_ellipse= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+	# dilation3 = cv2.dilate(filtered,kernel_ellipse,iterations = 1)
+	# median = cv2.medianBlur(dilation2,5)
+	# ret,thresh = cv2.threshold(median,10,1,cv2.THRESH_BINARY)
 
-	mul = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	# mul = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	# print('before:',mul[0:10][0:10])
 	cv2.imshow('mulgray',mul)
 	mul = np.multiply(mul,thresh)
@@ -120,55 +120,55 @@ while(cap.isOpened()):
 
 	cv2.imshow('mul',mul)
 
-	#Find contours of the filtered frame
-	_, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-	#Find Max contour area (Assume that hand is in the frame)
-	max_area=100
-	ci=0
-	for i in range(len(contours)):
-		cnt=contours[i]
-		area = cv2.contourArea(cnt)
-		if(area>max_area):
-			max_area=area
-			ci=i
-	#Largest area contour
-	if len(contours)== 0:
-		continue
-	cnts = contours[ci]
-	#Find convex hull
-	hull = cv2.convexHull(cnts)
-	# #Find convex defects
-	# hull2 = cv2.convexHull(cnts,returnPoints = False)
-	# defects = cv2.convexityDefects(cnts,hull2)
-	# #Get defect points and draw them in the original image
-	# FarDefect = []
-	# for i in range(defects.shape[0]):
-	# 	s,e,f,d = defects[i,0]
-	# 	start = tuple(cnts[s][0])
-	# 	end = tuple(cnts[e][0])
-	# 	far = tuple(cnts[f][0])
-	# 	FarDefect.append(far)
-	# 	# cv2.line(img,start,end,[0,255,0],1)
-	# 	# cv2.circle(img,far,10,[100,255,255],3)
+	# #Find contours of the filtered frame
+	# _, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	# #Find Max contour area (Assume that hand is in the frame)
+	# max_area=100
+	# ci=0
+	# for i in range(len(contours)):
+	# 	cnt=contours[i]
+	# 	area = cv2.contourArea(cnt)
+	# 	if(area>max_area):
+	# 		max_area=area
+	# 		ci=i
+	# #Largest area contour
+	# if len(contours)== 0:
+	# 	continue
+	# cnts = contours[ci]
+	# #Find convex hull
+	# hull = cv2.convexHull(cnts)
+	# # #Find convex defects
+	# # hull2 = cv2.convexHull(cnts,returnPoints = False)
+	# # defects = cv2.convexityDefects(cnts,hull2)
+	# # #Get defect points and draw them in the original image
+	# # FarDefect = []
+	# # for i in range(defects.shape[0]):
+	# # 	s,e,f,d = defects[i,0]
+	# # 	start = tuple(cnts[s][0])
+	# # 	end = tuple(cnts[e][0])
+	# # 	far = tuple(cnts[f][0])
+	# # 	FarDefect.append(far)
+	# # 	# cv2.line(img,start,end,[0,255,0],1)
+	# # 	# cv2.circle(img,far,10,[100,255,255],3)
 
 
-	# Print bounding rectangle
-	x,y,w,h = cv2.boundingRect(cnts)
-	img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-	roi = mul[y:y+h,x:x+w]
-	pic = np.zeros((120,180),np.uint8)
-	# roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-	cv2.imshow('pic',pic)
-	if w/180 < h/120 :
-		width = int(120*w/h)
-		roi = cv2.resize(roi,(width,120),interpolation=cv2.INTER_CUBIC)
-		height = h
-		pic[0:120,90-int(width/2):90-int(width/2)+width] = roi
-	# else:
-	# 	height = int(180*h/w)
-	# 	roi = cv2.resize(roi,(180,height),interpolation=cv2.INTER_CUBIC)
-	# 	width = w
-	# 	pic[0:height,0:120] = roi
+	# # Print bounding rectangle
+	# x,y,w,h = cv2.boundingRect(cnts)
+	# img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+	# roi = mul[y:y+h,x:x+w]
+	# pic = np.zeros((120,180),np.uint8)
+	# # roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+	# cv2.imshow('pic',pic)
+	# if w/180 < h/120 :
+	# 	width = int(120*w/h)
+	# 	roi = cv2.resize(roi,(width,120),interpolation=cv2.INTER_CUBIC)
+	# 	height = h
+	# 	pic[0:120,90-int(width/2):90-int(width/2)+width] = roi
+	# # else:
+	# # 	height = int(180*h/w)
+	# # 	roi = cv2.resize(roi,(180,height),interpolation=cv2.INTER_CUBIC)
+	# # 	width = w
+	# # 	pic[0:height,0:120] = roi
 	if np.count_nonzero(pic) == 0:
 		continue
 	descriptor,labels=SIFTtest(pic,kmeans,count)
